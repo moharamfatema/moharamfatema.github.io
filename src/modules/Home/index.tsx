29faf66b2f93ffae7bd1329edfacd56c42b11183
Fatema Moharam <moharamfatema@gmail.com>
2024-03-02 23:59:57 +0200

    modern navbar

--- src/modules/Home/index.tsx
+++ src/modules/Home/index.tsx
@@ -5,11 +5,11 @@ import Summary from './components/Summary'
 const Home : FC= () => {
 
     return (
-        <div className="home flex flex-col lg:grid lg:grid-cols-3 gap-9" id='home'>
-            <Picture/>
+        <div className="home flex flex-col lg:grid lg:grid-cols-3 gap-9 mt-4" id='home'>
             <div>
                 <Summary/>
             </div>
+            <Picture/>
         </div>
     )
 }
--- src/modules/Navbar/index.tsx
+++ src/modules/Navbar/index.tsx
@@ -30,8 +30,8 @@ const Navbar : React.FC = () => {
     })
 
     return (
-        <div>
-            <nav className="navbar bg-background-darker w-[100vw] shadow-lg shadow-black" id="mainNav">
+        <div className="pt-2 px-5 fixed w-full z-50">
+            <nav className={'navbar lg:rounded-full bg-background-darker shadow-md shadow-purple-800   '+ (active.icon === 'fa-bars' ? 'rounded-full':'rounded-3xl')} id="mainNav">
                 <div className="">
                     <button className="navbar-toggler navbar-toggler-right p-2 lg:hidden text-center w-full" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation" onClick={
                         () => {
@@ -43,14 +43,12 @@ const Navbar : React.FC = () => {
                     }>
                         <i className={'fa '+active.icon}></i>
                     </button>
-                    <div className="navbar-line w-full border-b-2 border-b-purple-600">
-                    </div>
-                    <div className={'navbar-collapse bg-background-darker w-full text-center lg:block'+active.show} id="navbarResponsive">
-                        <ul className={'navbar-nav uppercase ml-auto lg:flex '+active.show}>
+                    <div className={'rounded-lg overflow-clip lg:rounded-full navbar-collapse  w-full text-center lg:block'+active.show} id="navbarResponsive">
+                        <ul className={'navbar-nav uppercase ml-auto lg:flex transition-all '+active.show}>
                             {
                                 hashes.map((hash, index) => {
                                     return (
-                                        <li className="nav-item border-b-2 border-b-purple-600 hover:bg-background-dark w-full h-full" key={index}>
+                                        <li className="nav-item hover:bg-background-dark w-full h-full" key={index}>
                                             <a href={hash.hash} className='p-4'>
                                                 {hash.name}
                                             </a>
